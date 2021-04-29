d8a9147a427fd4063b2c7fdc99730ad26b5312db
Satyrr <gruzi12@gmail.com>
2021-04-29 13:40:21 +0200

    Rename and move run.py

--- character_classifier/experiments.py
+++ character_classifier/experiments.py
@@ -4,7 +4,7 @@ from character_classifier.src.models.models.efficientnet import EfficientNet
 from functools import partial
 
 if __name__ == '__main__':
-    train(model_cls=SimpleCNN, num_epochs=10, lr=0.001)
+    train(model_cls=SimpleCNN, num_epochs=3, lr=0.001, kernel_size_1=5, filters_num_2=50)
 
     #eff_net_cls = partial(EfficientNet, 'tf_efficientnet_b0_ns', True)
     #train(model_cls=eff_net_cls, num_epochs=10, lr=0.0001, batch_size=4)
